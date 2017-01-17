22e5c6d9259b880d672df9e889956ba2ce9dcc82
Tomasz Urbas <tomasz.urbas@fingo.pl>
2017-01-17 15:50:55 +0100

    fixed HolidayService test

--- src/test/java/info/fingo/urlopia/holidays/HolidayServiceTest.java
+++ src/test/java/info/fingo/urlopia/holidays/HolidayServiceTest.java
@@ -9,6 +9,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Jakub Licznerski
@@ -16,19 +17,6 @@ import java.util.List;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class HolidayServiceTest {
-
-    //inner class mocks equals method for testing convenience
-    private class HolidayMock extends Holiday {
-
-        public HolidayMock (String name, LocalDate date){
-            super(name, date);
-        }
-
-        @Override
-        public boolean equals (Object holiday){
-            return this.getDate().equals(((Holiday)holiday).getDate());
-        }
-    }
 
     @InjectMocks
     private HolidayService holidayService;
@@ -43,30 +31,21 @@ public class HolidayServiceTest {
 
     @Test
     public void generateHolidaysTest() {
-        List<Holiday> holidayList = holidayService.generateHolidaysList(LocalDate.now().getYear());
-        holidayList.addAll(holidayService.generateHolidaysList(LocalDate.now().getYear()+1));
+        int currentYear = LocalDate.now().getYear();
 
-        List<HolidayMock> test_holidays = new ArrayList<>(holidayList.size());
+        List<Holiday> holidayList = holidayService.generateHolidaysList(currentYear);
+        holidayList.addAll(holidayService.generateHolidaysList(currentYear + 1));
+        List<LocalDate> holidayDates = holidayList.stream().map(Holiday::getDate).collect(Collectors.toList());
+
+        List<LocalDate> test_holidayDates = new ArrayList<>(holidayList.size());
         final int[][] static_holidays = {{1, 1}, {1, 6}, {5, 1}, {5, 3}, {8, 15}, {11, 1}, {11, 11}, {12, 25}, {12, 26}};
 
-
         for (int i = 0; i < static_holidays.length; i++) {
-            test_holidays.add(new HolidayMock("", LocalDate.of(2016, static_holidays[i][0], static_holidays[i][1])));
-            test_holidays.add(new HolidayMock("", LocalDate.of(2017, static_holidays[i][0], static_holidays[i][1])));
+            test_holidayDates.add(LocalDate.of(currentYear, static_holidays[i][0], static_holidays[i][1]));
+            test_holidayDates.add(LocalDate.of(currentYear + 1, static_holidays[i][0], static_holidays[i][1]));
         }
 
-        test_holidays.add(new HolidayMock("", LocalDate.of(2016, 3, 27)));
-        test_holidays.add(new HolidayMock("", LocalDate.of(2016, 3, 28)));
-        test_holidays.add(new HolidayMock("", LocalDate.of(2016, 5, 15)));
-        test_holidays.add(new HolidayMock("", LocalDate.of(2016, 5, 26)));
-
-        test_holidays.add(new HolidayMock("", LocalDate.of(2017, 4, 16)));
-        test_holidays.add(new HolidayMock("", LocalDate.of(2017, 4, 17)));
-        test_holidays.add(new HolidayMock("", LocalDate.of(2017, 6, 4)));
-        test_holidays.add(new HolidayMock("", LocalDate.of(2017, 6, 15)));
-
-        Assert.assertTrue(holidayList.containsAll(test_holidays)); //collection.contains(h) uses h.equals(collection_element)
-
+        Assert.assertTrue(holidayDates.containsAll(test_holidayDates)); //collection.contains(h) uses h.equals(collection_element)
     }
 
 }
