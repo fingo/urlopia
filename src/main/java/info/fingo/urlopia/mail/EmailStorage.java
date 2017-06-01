d05c12780c7b37837f2061168b118deb72ace8df
Tomasz Urbas <tomasz.urbas@fingo.pl>
2017-06-01 11:47:30 +0200

    remove dash in properties file

--- src/main/java/info/fingo/urlopia/mail/EmailStorage.java
+++ src/main/java/info/fingo/urlopia/mail/EmailStorage.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class EmailStorage {
 
-    @Value("${email.storage.vacations-ec}")
+    @Value("${email.storage.vacations.ec}")
     private String ecVacationsStorage;
 
     @Autowired
