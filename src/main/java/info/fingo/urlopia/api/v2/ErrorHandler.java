e145cc8114573df8c84daef9fac12a4174d6851b
Mateusz Uzarek <mateusz.uzarek@fingo.pl>
2021-08-03 10:21:00 +0200

    https://jira.fingo.info/browse/PRAK-52 Implement Session part

--- src/main/java/info/fingo/urlopia/api/v2/ErrorHandler.java
+++ src/main/java/info/fingo/urlopia/api/v2/ErrorHandler.java
@@ -1,4 +1,4 @@
-package info.fingo.urlopia.apiv2;
+package info.fingo.urlopia.api.v2;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
