adde9e6541b783b3c9e3b81993fe42d15405496c
DaDudek <dawiddudekstg@gmail.com>
2021-08-31 21:36:16 +0200

    https://jira.fingo.info/browse/PRAK-126 All special absences should be visible during weekend in working time evidence report

--- src/main/java/info/fingo/urlopia/reports/evidence/params/resolver/handlers/day/params/resolver/EvidenceReportWeekendHandler.java
+++ src/main/java/info/fingo/urlopia/reports/evidence/params/resolver/handlers/day/params/resolver/EvidenceReportWeekendHandler.java
@@ -1,24 +1,17 @@
 package info.fingo.urlopia.reports.evidence.params.resolver.handlers.day.params.resolver;
 
-import info.fingo.urlopia.request.Request;
 import info.fingo.urlopia.request.RequestService;
+import info.fingo.urlopia.request.RequestType;
 import info.fingo.urlopia.request.absence.OperationNotSupportedException;
-import info.fingo.urlopia.request.absence.SpecialAbsenceReason;
 import info.fingo.urlopia.user.User;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @RequiredArgsConstructor
 public class EvidenceReportWeekendHandler {
     private static final String SUNDAY_STATUS = "wn";
     private static final String SATURDAY_STATUS = "ws";
-    private static final List<SpecialAbsenceReason> SPECIAL_REQUESTS_TYPES_NEEDS_TO_BE_HANDLE =
-            List.of(SpecialAbsenceReason.CHILDCARE,
-                    SpecialAbsenceReason.PARENTAL_LEAVE,
-                    SpecialAbsenceReason.MATERNITY_LEAVE,
-                    SpecialAbsenceReason.PATERNITY_LEAVE);
     private final RequestService requestService;
     private final EvidenceReportStatusFromRequestHandler evidenceReportStatusFromRequestHandler;
 
@@ -28,7 +21,7 @@ public class EvidenceReportWeekendHandler {
         return requestService
                 .getByUserAndDate(user.getId(), date)
                 .stream()
-                .filter(this::checkIfRequestNeedsToBeProcessed)
+                .filter(request -> request.getType() == RequestType.SPECIAL)
                 .map(evidenceReportStatusFromRequestHandler::handle)
                 .findFirst()
                 .orElse(getEvidenceReportStatusFromWeekendDay(date));
@@ -42,19 +35,6 @@ public class EvidenceReportWeekendHandler {
             case SUNDAY -> SUNDAY_STATUS;
             default -> throw OperationNotSupportedException.operationNotSupported();
         };
-    }
-
-    private boolean checkIfRequestNeedsToBeProcessed(Request request){
-        var requestType =request.getType();
-        return switch (requestType){
-            case NORMAL, OCCASIONAL -> false;
-            case SPECIAL -> checkIfSpecialRequestNeedsToBeProcessed(request);
-        };
-    }
-
-    private boolean checkIfSpecialRequestNeedsToBeProcessed(Request request){
-        var specialAbsenceReason = SpecialAbsenceReason.valueOf(request.getSpecialTypeInfo());
-        return SPECIAL_REQUESTS_TYPES_NEEDS_TO_BE_HANDLE.contains(specialAbsenceReason);
     }
 
 }
