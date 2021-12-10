e8947df50b1931bee69f356f643d038390b68604
Dawid Dudek <dawid.dudek@fingo.pl>
2021-12-10 13:37:48 +0100

    https://jira.fingo.info/browse/IJ-209 add all the data you have to the monthly report

--- src/main/java/info/fingo/urlopia/api/v2/reports/attendance/resolver/handlers/user/params/resolver/MonthlyAttendanceListReportDayHandler.java
+++ src/main/java/info/fingo/urlopia/api/v2/reports/attendance/resolver/handlers/user/params/resolver/MonthlyAttendanceListReportDayHandler.java
@@ -13,6 +13,8 @@ import java.time.LocalDate;
 
 public class MonthlyAttendanceListReportDayHandler {
     private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat();
+    private static final String DEFAULT_VALUE = "-";
+    private static final String EMPTY_VALUE = "";
     private final HolidayService holidayService;
     private final RequestService requestService;
     private final PresenceConfirmationService presenceConfirmationService;
@@ -30,44 +32,58 @@ public class MonthlyAttendanceListReportDayHandler {
                          int month,
                          int dayOfMonth,
                          User user) {
+        if (user == null) {
+            return EMPTY_VALUE;
+        }
         var currentDate = LocalDate.now();
         try {
-            var handleDate = LocalDate.of(year,month,dayOfMonth);
+            var handleDate = LocalDate.of(year, month, dayOfMonth);
             var isDateInPast = handleDate.isBefore(currentDate);
 
-            if (user == null || !isDateInPast) {
-                return "";
-            }
+
             if (holidayService.isWorkingDay(handleDate)) {
-                return requestService
-                        .getByUserAndDate(user.getId(), handleDate).stream()
-                        .filter(req -> req.getStatus() == Request.Status.ACCEPTED)
-                        .map(this::handleRequest)
-                        .findFirst()
-                        .orElse(handlePresence(handleDate,user));
+                var resolvedValue = requestService
+                                    .getByUserAndDate(user.getId(), handleDate).stream()
+                                    .filter(req -> req.getStatus() == Request.Status.ACCEPTED)
+                                    .map(this::handleRequest)
+                                    .findFirst()
+                                    .orElse(handlePresence(handleDate, user));
+                if (shouldReturnEmptyValue(resolvedValue, isDateInPast)) {
+                    return EMPTY_VALUE;
+                }
+                return resolvedValue;
             }
         } catch (DateTimeException e) {
             // if day does not exist then default value
         }
-        return "-";
+        return DEFAULT_VALUE;
     }
 
-    private String handleRequest(Request request){
+    private String handleRequest(Request request) {
         var requestType = request.getType();
         var specialTypeInfo = request.getSpecialTypeInfo();
         return switch (requestType) {
             case NORMAL -> ReportStatusFromRequestType.NORMAL.getMonthlyPresenceReportStatus();
             case OCCASIONAL -> ReportStatusFromRequestType.OCCASIONAL.getMonthlyPresenceReportStatus();
-            case SPECIAL ->  ReportStatusFromRequestType.valueOf(specialTypeInfo).getMonthlyPresenceReportStatus();
+            case SPECIAL -> ReportStatusFromRequestType.valueOf(specialTypeInfo).getMonthlyPresenceReportStatus();
         };
     }
 
     private String handlePresence(LocalDate date,
-                                  User user){
-        var presenceConfirmations = presenceConfirmationService.getByUserAndDate(user.getId(),date);
-        if(presenceConfirmations.isEmpty()){
-            return "-";
+                                  User user) {
+        var presenceConfirmations = presenceConfirmationService.getByUserAndDate(user.getId(), date);
+        if (presenceConfirmations.isEmpty()) {
+            return DEFAULT_VALUE;
         }
         return DECIMAL_FORMAT.format(presenceConfirmationService.countWorkingHoursInDay(presenceConfirmations.get(0)));
+    }
+
+    private boolean isDefault(String resolvedValue) {
+        return DEFAULT_VALUE.equals(resolvedValue);
+    }
+
+    private boolean shouldReturnEmptyValue(String resolvedValue,
+                                           boolean isDayInPast) {
+        return isDefault(resolvedValue) && !isDayInPast;
     }
 }
