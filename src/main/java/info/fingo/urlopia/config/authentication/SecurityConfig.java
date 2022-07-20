d4ca50422e5039c526cac9c769a93bfd1478ec12
Dawid Dudek <dawid.dudek@fingo.pl>
2022-07-20 16:04:24 +0200

    https://jira.fingo.info/browse/IJ-245 Add spring security

--- src/main/java/info/fingo/urlopia/config/authentication/SecurityConfig.java
+++ src/main/java/info/fingo/urlopia/config/authentication/SecurityConfig.java
@@ -2,11 +2,14 @@ package info.fingo.urlopia.config.authentication;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Arrays;
 
 
 @EnableGlobalMethodSecurity(
@@ -19,8 +22,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final JwtFilter jwtFilter;
 
+    private final Environment environment;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        var activeProfiles = Arrays.asList(environment.getActiveProfiles());
+
+        mainHttpConfiguration(http);
+
+        if (activeProfiles.contains("developer")) {
+            enableH2ConsoleAccess(http);
+        }
+    }
+
+    private void mainHttpConfiguration(HttpSecurity http) throws Exception {
         http.cors();
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
@@ -34,4 +49,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
         http.exceptionHandling().accessDeniedHandler(new AccessDeniedExceptionHandler());
     }
+
+    private void enableH2ConsoleAccess(HttpSecurity http) throws Exception {
+        http
+                .headers()
+                .frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/h2-console/**").permitAll();
+    }
+
 }
