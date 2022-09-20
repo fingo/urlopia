c7589822e24a66db1154394f2358109924854bea
Witold Brzozowski <witold.brzozowski@fingo.pl>
2022-09-20 17:41:04 +0200

    Move tenant id to env

--- view.react/src/authConfig.js
+++ view.react/src/authConfig.js
@@ -1,7 +1,7 @@
 export const msalConfig = {
     auth: {
         clientId: process.env.REACT_APP_OAUTH_CLIENT_ID,
-        authority: 'https://login.microsoftonline.com/09a1625a-533f-428a-b137-b8d1f46d688a',
+        authority: `https://login.microsoftonline.com/${process.env.REACT_APP_OAUTH_TENANT_ID}`,
         redirectUri: `${window.location.origin}/login_check`,
     },
     cache: {
