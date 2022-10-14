8a750ada5e1072757ef3bd3e2bf66347b1dde7ac
Dawid Dudek <dawid.dudek@fingo.pl>
2022-10-14 13:40:46 +0200

    https://jira.fingo.info/browse/IJ-274 Year evidence report not work correct for user who switches from b2b to ec or vice versa.

--- src/main/java/info/fingo/urlopia/request/RequestRepository.java
+++ src/main/java/info/fingo/urlopia/request/RequestRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface RequestRepository extends BaseRepository<Request>, JpaRepository<Request, Long> {
@@ -18,5 +19,18 @@ public interface RequestRepository extends BaseRepository<Request>, JpaRepositor
             """)
     List<Request> findByRequesterIdAndYear(@Param("requesterId") Long requesterId,
                                            @Param("year") Integer year);
+
+    @Query(value = """
+            SELECT *
+            FROM requests r
+            WHERE r.requester_id = :requesterId AND
+            r.status = :status AND
+            (r.start_date, r.end_date) overlaps (:startDate, :endDate);
+            """, nativeQuery = true)
+    List<Request> findByRequesterAndDateIntervalAndStatus(@Param("requesterId") Long requesterId,
+                                                          @Param("startDate") LocalDate startDate,
+                                                          @Param("endDate") LocalDate endDate,
+                                                          @Param("status") String status);
+//
 
 }
