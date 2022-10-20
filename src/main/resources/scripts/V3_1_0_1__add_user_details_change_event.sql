1f4eb11a546f414d6961683340cd6ccafae85039
Dawid Dudek <dawid.dudek@fingo.pl>
2022-10-20 09:18:25 +0200

    improve flyway to be compatible with more postgres versions

--- src/main/resources/scripts/V3_1_0_1__add_user_details_change_event.sql
+++ src/main/resources/scripts/V3_1_0_1__add_user_details_change_event.sql
@@ -1,7 +1,7 @@
 ALTER TABLE history_logs ADD COLUMN user_details_change_event varchar(60) DEFAULT NULL;
 
 INSERT INTO history_logs (created, user_id, user_work_time, hours,  "comment",	user_details_change_event)
-SELECT hl2.created - interval '2 seconds', hl2.user_id, hl2.user_work_time, hl2.hours, CONCAT('Zmieniono etat z: ', ROUND(hl.user_work_time / 8.0 , 2) , ' na ', ROUND(hl2.user_work_time/8.0 , 2)), 'USER_CHANGE_WORK_TIME'
+SELECT hl2.created - interval '2 seconds', hl2.user_id, hl2.user_work_time, hl2.hours, CONCAT('Zmieniono etat z: ', ROUND(CAST(hl.user_work_time AS numeric) / 8.0 , 2) , ' na ', ROUND(CAST(hl2.user_work_time AS numeric) / 8.0 , 2)), 'USER_CHANGE_WORK_TIME'
 FROM history_logs hl
     JOIN history_logs hl2 ON (hl.id = hl2.prev_history_log_id AND hl.user_id = hl2.user_id )
 WHERE hl.user_work_time != hl2.user_work_time;
--- view.react/src/contexts/absence-history-context/actions/fetchUserAbsenceHistory.js
+++ view.react/src/contexts/absence-history-context/actions/fetchUserAbsenceHistory.js
@@ -2,7 +2,7 @@ import {sendGetRequest} from "../../../helpers/RequestHelper";
 import {FETCH_USER_RECENT_ABSENCE_HISTORY_ACTION_PREFIX, FETCH_USER_RECENT_ABSENCE_HISTORY_URL} from "../constants";
 
 export const fetchRecentUserAbsenceHistory = (dispatch, userId) => {
-    const parameters = "page=0&size=5&sort=id,desc"
+    const parameters = "page=0&size=5&sort=created,desc"
     return fetchUserAbsenceHistory(dispatch, userId, parameters, {recent: true})
 }
 export const fetchPagedUserAbsenceHistory = (
