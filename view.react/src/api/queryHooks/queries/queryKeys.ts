d9129d6897bc54677fe02031065320990adbf4c8
Dawid Dudek <dawid.dudek@fingo.pl>
2022-12-13 09:08:12 +0100

    [IJ-300] API Abstraction and main calendar request

--- view.react/src/api/queryHooks/queries/queryKeys.ts
+++ view.react/src/api/queryHooks/queries/queryKeys.ts
@@ -1,5 +1,5 @@
 export const calendarKeys = {
-  all: ["dataSource"] as const,
+  all: ["calendar"] as const,
   lists: () => [...calendarKeys.all, "list"] as const,
   list: ({ startDate, endDate }: { startDate: string; endDate: string }) =>
     [...calendarKeys.lists(), { startDate, endDate }] as const,
