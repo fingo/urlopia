54ceb056ff3a733d58f7454d4854f443afeb561c
sswiderski <szymon.swiderski@tes.com>
2023-06-05 09:50:41 +0200

    Add material icons and delete redundant code

--- view.react/src/components/sidebar/Sidebar.jsx
+++ view.react/src/components/sidebar/Sidebar.jsx
@@ -10,16 +10,11 @@ import WatchLaterRoundedIcon from '@mui/icons-material/WatchLaterRounded';
 import WorkHistoryOutlinedIcon from '@mui/icons-material/WorkHistoryOutlined';
 import classNames from "classnames";
 import PropTypes from 'prop-types';
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import {Container, Nav} from 'react-bootstrap';
 
 import {getCurrentUser} from "../../api/services/session.service";
-import {useAppInfo} from "../../contexts/app-info-context/appInfoContext";
-import {fetchPendingDays} from "../../contexts/vacation-days-context/actions/fetchPendingDays";
-import {fetchVacationDays} from "../../contexts/vacation-days-context/actions/fetchVacationDays";
-import {useVacationDays} from "../../contexts/vacation-days-context/vacationDaysContext";
 import {AttentionIcon, TextWithIcon} from "../../helpers/icons/Icons";
-import {formatHoursToDays} from "../../helpers/RemainingDaysFormatterHelper";
 import {Link} from "./link/Link";
 import styles from './Sidebar.module.scss';
 
@@ -27,45 +22,6 @@ export const Sidebar = ({onClickLinkOrOutside, acceptancesPresent}) => {
     const {isAdmin: isUserAnAdmin, isLeader: isUserALeader} = getCurrentUser();
     const {ec: isUserEC} = getCurrentUser();
     const overlayClass = classNames(styles.overlay, 'd-lg-none');
-
-    const [vacationDays, setVacationDays] = useState(0);
-    const [vacationHours, setVacationHours] = useState(0);
-    const [pendingDays, setPendingDays] = useState(0);
-    const [pendingHours, setPendingHours] = useState(0);
-    const [workTime, setWorkTime] = useState(8);
-
-    const [vacationDaysState, vacationDaysDispatch] = useVacationDays();
-
-    const [appInfoState, ] = useAppInfo()
-    const {version, commitId} = appInfoState.appInfo
-
-    useEffect(() => {
-        fetchPendingDays(vacationDaysDispatch);
-    }, [vacationDaysDispatch]);
-
-    useEffect(() => {
-        fetchVacationDays(vacationDaysDispatch);
-    }, [vacationDaysDispatch]);
-
-    useEffect(() => {
-        const {pendingDays, pendingHours} = vacationDaysState.pendingDays;
-        setPendingDays(pendingDays);
-        setPendingHours(pendingHours);
-    }, [vacationDaysState.pendingDays]);
-
-    useEffect(() => {
-        const {remainingDays, remainingHours, workTime} = vacationDaysState.vacationDays;
-        setVacationDays(remainingDays);
-        setVacationHours(remainingHours);
-        setWorkTime(workTime);
-    }, [vacationDaysState.vacationDays]);
-
-    const remainingDays = vacationDays-pendingDays
-    const remainingHours = vacationHours - pendingHours
-    const remainingHoursAsDays = formatHoursToDays(remainingHours/workTime)
-    const pendingHoursAsDays  = formatHoursToDays(pendingHours/workTime)
-
-    const leaveLabel = isUserEC ? "Pozostały urlop: " : "Pozostała przerwa: "
 
     const [click, setClick] = useState(false);
     const handleClick = () => setClick(!click);
