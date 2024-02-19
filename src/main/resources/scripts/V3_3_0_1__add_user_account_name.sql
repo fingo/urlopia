b2d59d9c9e71ebfefdee3a367366d090da598067
Witold Brzozowski <brzozowskyy@gmail.com>
2024-02-19 14:07:59 +0100

    Fix account name migration (#239)

--- src/main/resources/scripts/V3_3_0_1__add_user_account_name.sql
+++ src/main/resources/scripts/V3_3_0_1__add_user_account_name.sql
@@ -1,1 +1,7 @@
-ALTER TABLE users ADD COLUMN account_name VARCHAR(63) NOT NULL UNIQUE;
+ALTER TABLE users ADD COLUMN account_name VARCHAR(63);
+
+UPDATE users SET account_name = SUBSTRING(mail FROM 1 FOR POSITION('@' IN mail) - 1);
+
+ALTER TABLE users ALTER COLUMN account_name SET NOT NULL;
+
+CREATE UNIQUE INDEX users_account_name_index ON users (account_name);
