0b478739415d379b6dc952186cdb909cc357074b
witold.brzozowski <witold.brzozowski@fingo.net>
2024-10-22 17:27:32 +0200

    Allow for non-unique principal name

--- src/main/java/info/fingo/urlopia/user/User.java
+++ src/main/java/info/fingo/urlopia/user/User.java
@@ -19,7 +19,7 @@ public class User {
     @Column(nullable = false, unique = true)
     private String accountName;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String principalName;
 
     @Column(nullable = false, unique = true)
---
+++ src/main/resources/scripts/U3_5_0_4__drop_principal_name_unique_constraint.sql
@@ -0,0 +1,2 @@
+CREATE UNIQUE INDEX users_principal_name_index ON users (principal_name);
+CREATE UNIQUE INDEX users_principal_name_key ON users (principal_name);
