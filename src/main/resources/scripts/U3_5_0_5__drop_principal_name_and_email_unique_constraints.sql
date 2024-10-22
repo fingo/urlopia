12d3538ac185f56aaebdd9c48ccbda84cf801281
witold.brzozowski <witold.brzozowski@fingo.net>
2024-10-22 17:56:19 +0200

    Drop unnecessary unique constraints

---
+++ src/main/resources/scripts/U3_5_0_5__drop_principal_name_and_email_unique_constraints.sql
@@ -0,0 +1,4 @@
+CREATE UNIQUE INDEX users_mail_index ON users (mail);
+CREATE UNIQUE INDEX users_mail_key ON users (mail);
+CREATE UNIQUE INDEX users_principal_name_index ON users (principal_name);
+CREATE UNIQUE INDEX users_principal_name_key ON users (principal_name);
---
+++ src/main/resources/scripts/V3_5_0_5__drop_principal_name_and_email_unique_constraints.sql
@@ -0,0 +1,4 @@
+DROP CONSTRAINT IF EXISTS users_mail_index;
+DROP CONSTRAINT IF EXISTS users_mail_key;
+DROP CONSTRAINT IF EXISTS users_principal_name_index;
+DROP CONSTRAINT IF EXISTS users_principal_name_key;
