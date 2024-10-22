28aff99bd97d9ab829844086b610c324310bb159
boavenn <brzozowskyy@gmail.com>
2024-10-22 14:33:29 +0100

    Add more logs to auth related classes (#246)

--- src/main/java/info/fingo/urlopia/config/authentication/AccessDeniedExceptionHandler.java
+++ src/main/java/info/fingo/urlopia/config/authentication/AccessDeniedExceptionHandler.java
@@ -1,5 +1,7 @@
 package info.fingo.urlopia.config.authentication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
@@ -11,10 +13,13 @@ import java.io.IOException;
 
 public class AccessDeniedExceptionHandler implements AccessDeniedHandler
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AccessDeniedExceptionHandler.class);
+
     @Override
     public void handle(HttpServletRequest request,
                        HttpServletResponse response,
                        AccessDeniedException ex) throws IOException, ServletException {
+        LOGGER.warn("Access denied", ex);
         response.setStatus(HttpStatus.FORBIDDEN.value());
     }
 }
