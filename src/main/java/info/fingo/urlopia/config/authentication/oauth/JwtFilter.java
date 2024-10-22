6f5c1e9a807801bdb7f573be3f53f8297389f872
witold.brzozowski <witold.brzozowski@fingo.net>
2024-10-22 16:03:57 +0200

    Revert "Synchronize users by account name"
    This reverts commit 0219d72f

--- src/main/java/info/fingo/urlopia/config/authentication/oauth/JwtFilter.java
+++ src/main/java/info/fingo/urlopia/config/authentication/oauth/JwtFilter.java
@@ -4,8 +4,6 @@ package info.fingo.urlopia.config.authentication.oauth;
 import info.fingo.urlopia.api.v2.authentication.oauth.OAuthRedirectService;
 import info.fingo.urlopia.user.NoSuchUserException;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -23,7 +21,6 @@ import java.io.IOException;
 @RequiredArgsConstructor
 @ConditionalOnProperty(name = "ad.configuration.enabled", havingValue = "true", matchIfMissing = true)
 public class JwtFilter extends OncePerRequestFilter {
-    private static final Logger LOGGER = LoggerFactory.getLogger(OncePerRequestFilter.class);
     private final JwtTokenValidator jwtTokenValidator;
 
     @Override
@@ -49,7 +46,6 @@ public class JwtFilter extends OncePerRequestFilter {
             var authorities = accessToken.getAuthorities();
             return new UsernamePasswordAuthenticationToken(accountName, null, authorities);
         }catch (InvalidTokenException | NoSuchUserException exception){
-            LOGGER.warn("Invalid authentication token", exception);
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return null;
         }
